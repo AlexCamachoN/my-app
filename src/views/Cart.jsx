7382283d705246e449a1d211ff154a7fb8a73425
Alex <cnalex_2@hotmail.com>
2022-08-25 20:26:16 -0500

    finalizando cart

--- src/views/Cart.jsx
+++ src/views/Cart.jsx
@@ -2,25 +2,46 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import Compra from '../component/compra/Compra'
 import { useCart } from '../context/CartContext'
+import {FaWhatsapp } from "react-icons/fa";
 
 const Cart = () => {
-  const{cart, emptyCart, totalCart}= useCart()
+  const{cart,onAdd, emptyCart, totalCart}= useCart()
   const navegar = useNavigate()
+
+  function iniciarChatear(){
+    var products= ['zapato', 'mesa', 'silla']
+    window.location.href = 'https://wa.me/+51978374972/?text=' + ' ' + products[0] + ' ' + products[1] + ' ' + products[2]
+}
 
   return (
     <>
       {!cart.length?
-      <div>
-        <h2>Tu carrito esta vacio, te invitamos a ver nuestras obras de arte</h2>
-        <button onClick={()=> navegar('/filtroinput')}>Ir a productos</button>
+      <div style={{textAlign:"center"}}>
+        <h2>Tu carrito está vacío</h2>
+        <h3>Tenemos obras de arte originales a grandes precios</h3>
+        <button onClick={()=> navegar('/filtroinput')}>Ir a Obras de Arte</button>
+        <p>Bienvenido a nuesta galeria de Arte!</p>
       </div>
          :
-      <div >
+      <div style={{textAlign:"center"}} >
         <h2>Mi carrito</h2>
         {cart.map((compra)=> <Compra key={compra.id} compra={compra} /> )}
         <p>Total a pagar: ${totalCart()} </p>
         <button onClick={emptyCart}>Borrar carrito</button>
-        <button onClick={()=> navegar('/checkout')}>Finalizar compra</button>
+        <button >
+        {/* <button onClick={()=> navegar('/checkout')}> */}
+          {/* Finalizar compra */}
+        <a style={{color:'black', textDecoration:'none'}} 
+          target="_blank" rel="noreferrer" href='http://wa.me/+51978374972'
+          
+          >Termina la compra <FaWhatsapp/>
+        </a>
+        </button>
+        {/* <button type="button"  onclick={iniciarChatear}>Confirmar compra</button> */}
+        <div  >
+            
+            
+        </div>
       </div>}
     </>
   )
