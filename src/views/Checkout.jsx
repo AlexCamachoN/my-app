0455b12574546d74ba2886db348423e3a3bd8fb6
Alex <cnalex_2@hotmail.com>
2022-08-30 10:32:07 -0500

    terminado wasap

--- src/views/Checkout.jsx
+++ src/views/Checkout.jsx
@@ -3,23 +3,45 @@ import {FaWhatsapp } from "react-icons/fa";
 import PedidoCompleto from '../component/compra/PedidoCompleto';
 import { useCart } from '../context/CartContext';
 
+const Checkout = (props) => {
+let producto1=[];
+let precio1= [];
+let codigo= [];
+let total= 0;
 
-const Checkout = () => {
-  const{cart, totalCart}= useCart()
+  const{cart, totalCart,}= useCart()
+  for (let i = 0; i < cart.length; i++){
+  producto1[i]= cart[i].name
+  precio1[i]= cart[i].price 
+  codigo[i]= cart[i].codigo
+}
+  
+  //probando para pintar traer number por wasap
+  // const numbers=[1,2,3,4];
+  //   // const number = props.numbers;
+  //   const listItems = numbers.map((num) =>
+  //     <li key={num.toString()}>
+  //       {num}
+  //     </li>
+  //   );
+
   return (
     <div style={{textAlign:"center", minHeight:'60.5vh'}}>
       <div style={{margin:"20px",}}>
         <h3>Detalle de tu compra</h3>
         {cart.map((pedidocompleto)=> <PedidoCompleto key={pedidocompleto.id} pedidocompleto={pedidocompleto} /> )}
-        <p>Total a pagar: ${totalCart()} </p>
-      </div>
-
-
-      <button style={{background:"green"}}>
+        <p>Total a pagar: ${total= totalCart()} </p>
+      </div>   
+      <button style={{background:"green"}} >
       <a style={{color:'black', textDecoration:'none', background:"green"}} 
-          target="_blank" rel="noreferrer" href='http://wa.me/+51978374972'> 
-          <FaWhatsapp/>Completar Pedido en WhatsApp</a>
-      </button>
+          target="_blank" rel="noreferrer" href={`http://wa.me/+5194239308/?text=Tu pedido:%0A `
+           + producto1 + `%0A $:` + precio1 +`%0A codigo:`+ codigo  +`%0A Total :`+ total} 
+          > 
+          <FaWhatsapp/>Completar Pedido en WhatsApp</a> 
+
+         
+      </button>     
+      {/* <ul>{listItems}</ul> */}
     </div>
   )
 }
